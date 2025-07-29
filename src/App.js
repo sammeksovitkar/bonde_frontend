cb7bd10091400e5dc657522c8622b391a7089533
sammek <sovitkarsammek1998.com>
2025-07-29 18:05:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import LoginScreen from './component/LoginScreen';
 import TrackingPage from './TrackingPage';
 
 const App = () => {
   const [admin, setAdmin] = useState(null);
 
+  useEffect(() => {
+    const storedToken = localStorage.getItem('token');
+    if (storedToken) {
+      setAdmin({ token: storedToken });
+    }
+  }, []);
+
   const handleLoginSuccess = (adminData) => {
-    setAdmin(adminData); // adminData includes token
+    localStorage.setItem('token', adminData.token); // Store token
+    setAdmin(adminData);
   };
 
   return admin ? (
